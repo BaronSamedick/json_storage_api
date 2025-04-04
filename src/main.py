34061ea84dce9428e0d9ae227aa9ea3ec87d1bd2
ds.eruslanov <ds.eruslanov@tensor.ru>
2025-04-04 15:08:55 +0500

    * added start server from main file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
+
+from config import settings
 
 app = FastAPI()
 
@@ -6,3 +9,12 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello, World!!!"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        host=settings.server.host,
+        port=settings.server.port,
+        reload=settings.server.debug,
+    )
