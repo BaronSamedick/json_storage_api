f2a2d460916b691b4835ed03e7ddbcc8a6ebee2e
ds.eruslanov <ds.eruslanov@tensor.ru>
2025-04-08 15:20:55 +0500

    * added metrics endpoint for prometheus

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from config import settings
 from router import router as api_router
 
 app = FastAPI()
 app.include_router(api_router)
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == '__main__':
     uvicorn.run(
